923df0c384527fe9f096948bc05730aff6d644b4
choongang2022 <paran2586@naver.com>
2023-01-02 14:06:02 +0900

    회원가입 상세내용 추가, 상품 상세정보 조회

---
+++ server-main/src/main/java/com/laonworks/shop/api/controller/response/auth/ItemDetailResponse.java
@@ -0,0 +1,15 @@
+package com.laonworks.shop.api.controller.response.auth;
+
+import com.laonworks.shop.api.controller.response.BaseResponse;
+import com.laonworks.shop.api.controller.vo.ItemInfo;
+import com.laonworks.shop.api.mapper.vo.ItemVo;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ApiModel(description = "아이템 상세화면")
+public class ItemDetailResponse extends BaseResponse {
+    public ItemVo itemVo = null;
+}
