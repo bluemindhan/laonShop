e4c57d52e367a464dc4147c144c26d7cfe4ca240
choongang2022 <paran2586@naver.com>
2023-01-12 17:33:36 +0900

    [refactoring] 검색기능, main화면 찜 순 정렬기능

---
+++ web-main/src/service/response/MainPageResponse.js
@@ -0,0 +1,8 @@
+import BaseResponse from '@/service/response/BaseResponse';
+
+export default class MainPageResponse extends BaseResponse {
+    constructor() {
+        super();
+        this.volist = null;
+    }
+}
