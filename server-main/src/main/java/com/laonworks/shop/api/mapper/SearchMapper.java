3725a8a95e61825ff9803c61655df27338f180cd
choongang2022 <paran2586@naver.com>
2023-01-12 11:10:16 +0900

    상품 검색 기능 추가 (필터기능, 카테고리별, 키워드 검색)

--- server-main/src/main/java/com/laonworks/shop/api/mapper/SearchMapper.java
+++ server-main/src/main/java/com/laonworks/shop/api/mapper/SearchMapper.java
@@ -1,6 +1,7 @@
 package com.laonworks.shop.api.mapper;
 
 import com.laonworks.shop.api.controller.request.search.SearchRequest;
+import com.laonworks.shop.api.mapper.vo.ItemVo;
 import com.laonworks.shop.api.mapper.vo.ProductVo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
