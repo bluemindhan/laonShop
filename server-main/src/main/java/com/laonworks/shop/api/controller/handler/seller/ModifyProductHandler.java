94bdd9ed99a51453d4f8bf57e24648f9f9a7e36f
ANNEEEEEEE <rlawlgus8649@naver.com>
2023-01-20 11:45:50 +0900

    지현 상품수정 커밋

--- server-main/src/main/java/com/laonworks/shop/api/controller/handler/seller/ModifyProductHandler.java
+++ server-main/src/main/java/com/laonworks/shop/api/controller/handler/seller/ModifyProductHandler.java
@@ -14,14 +14,18 @@ import com.laonworks.shop.api.mapper.AuthMapper;
 import com.laonworks.shop.api.mapper.vo.UserVo;
 import com.laonworks.shop.api.service.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Component // 재사용이 가능한 독립된 모듈
 public class ModifyProductHandler extends BaseHandler {
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
     @Autowired
     AuthMapper authMapper;
 
@@ -54,7 +58,6 @@ public class ModifyProductHandler extends BaseHandler {
 
         try {
             UserVo sellerVo;
-            int result=0;
             if(userType == UserType.Seller.getValue()) {
                 sellerVo = authMapper.selectSellerInfo(email);
             }
@@ -64,24 +67,32 @@ public class ModifyProductHandler extends BaseHandler {
             }
             ProductVo productVo = new ProductVo();
             // 파라미터 setting
-            productVo.setSllrId(email);
+            productVo.setSllrId(sellerVo.userId);
             productVo.setPrdtNo(productNum);
             productVo.setPrdtNm(productName);
             productVo.setPrdtDesc(productDesc);
             productVo.setPrdtPrce(productPrice);
+            logger.info("1. productVo 확인" + productVo);
             delete = productMapper.deleteProductImg(productNum);
-            List<String> urlList = awsService.uploadProductImageList(req.imageList);
+//            List<String> urlList = awsService.uploadProductImageList(req.imageList);
 
-            if(req != null || !req.equals("")){
-               result=productService.modifyProduct(productVo, urlList);
-               log.info("result:" +result);
-               res.setResult(result);
-               res.setCode(ResultCode.Success);
-            }
-            else if(result == 0) {
+            List<String> urlList = new ArrayList<String>();
+            //urlList.add("https://btswallet-dev.s3.ap-northeast-2.amazonaws.com/product/838de7e3-f274-4ea1-8b42-c340e2495406.jpg"); // 파인애플
+            urlList.add("https://btswallet-dev.s3.ap-northeast-2.amazonaws.com/product/538b81b5-a7fa-44e1-9774-10c0be0a03a5.jpg"); //수박
+            // [https://btswallet-dev.s3.ap-northeast-2.amazonaws.com/product/9a9b49bc-a778-40bc-aafd-9d3544a4c965.jpg,
+            // https://btswallet-dev.s3.ap-northeast-2.amazonaws.com/product/838de7e3-f274-4ea1-8b42-c340e2495406.jpg,
+            // https://btswallet-dev.s3.ap-northeast-2.amazonaws.com/product/538b81b5-a7fa-44e1-9774-10c0be0a03a5.jpg]
+
+            int n = productService.modifyProduct(productVo, urlList);
+            logger.info("2. n" + n);
+            if(n == 0) {
                 res.setCode(ResultCode.InternalServerError);
+                res.result = 0;
                 return res;
             }
+            res.result = 1;
+            res.setCode(ResultCode.Success);
+            return res;
         }
         catch(Exception e) {
             res.setCode(ResultCode.InternalServerError);
